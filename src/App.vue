d5248df33859625da4e4dea934d0c6db45dc9c48
Eason <leoling127@gmail.com>
2019-05-13 23:58:28 +0800

    OK

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a
+        class="navbar-brand"
+        href="#"
+      >Full Stack Message Board</a>
+
+    </nav>
+    <router-view class="container" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+</style>
